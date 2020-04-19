ac6dfe5e2adbecc33304e6bc5c55d128b472fbe5
Oleg Marchenko <mos-webmaster@mail.ru>
2020-04-19 17:41:28 +0300

    Extracting sitemap loading mechanism

--- src/main/java/com/github/marchenkoprojects/sitemap4j/Sitemap.java
+++ src/main/java/com/github/marchenkoprojects/sitemap4j/Sitemap.java
@@ -1,21 +1,13 @@
 package com.github.marchenkoprojects.sitemap4j;
 
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.events.XMLEvent;
-import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.time.ZonedDateTime;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.function.BiConsumer;
 
 import static java.util.Objects.isNull;
-import static java.util.Objects.nonNull;
 
 /**
  * @author Oleg Marchenko
@@ -44,61 +36,7 @@ public class Sitemap {
         }
 
         new SitemapValidator().validate(file);
-
-        XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-        try {
-            Url.Builder urlBuilder = null;
-            BiConsumer<String, Url.Builder> tagValueConsumer = null;
-
-            XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new StreamSource(file));
-            while (xmlStreamReader.hasNext()) {
-                int event = xmlStreamReader.next();
-                if (event == XMLEvent.START_ELEMENT) {
-                    String tagName = xmlStreamReader.getLocalName();
-                    switch (tagName) {
-                        case "url":
-                            urlBuilder = Url.builder();
-                            break;
-                        case "loc":
-                            tagValueConsumer = (tagValue, builder) -> builder.setLoc(tagValue);
-                            break;
-                        case "lastmod":
-                            tagValueConsumer = (tagValue, builder) -> builder.setLastmod(ZonedDateTime.parse(tagValue));
-                            break;
-                        case "changefreq":
-                            tagValueConsumer = (tagValue, builder) -> builder.setChangefreq(ChangeFreqType.valueOf(tagValue.toUpperCase()));
-                            break;
-                        case "priority":
-                            tagValueConsumer = (tagValue, builder) -> builder.setPriority(Float.parseFloat(tagValue));
-                            break;
-                    }
-                }
-                if (event == XMLEvent.CHARACTERS) {
-                    String text = xmlStreamReader.getText().trim();
-                    if (nonNull(tagValueConsumer) && !text.isEmpty()) {
-                        tagValueConsumer.accept(text, urlBuilder);
-                    }
-                }
-                if (event == XMLEvent.END_ELEMENT) {
-                    String tagName = xmlStreamReader.getLocalName();
-                    switch (tagName) {
-                        case "url":
-                            urls.add(urlBuilder.build());
-                            urlBuilder = null;
-                            break;
-                        case "loc":
-                        case "lastmod":
-                        case "changefreq":
-                        case "priority":
-                            tagValueConsumer = null;
-                            break;
-                    }
-                }
-            }
-        }
-        catch (XMLStreamException e) {
-            throw new SitemapNotLoadedException(e);
-        }
+        new SitemapLoader().load(file, urls);
     }
 
     public boolean addUrl(Url url) {
